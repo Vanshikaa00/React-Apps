9cfb3e2043c792a0fe6bc7abe5820197b57a4d5a
Vanshikaa <vanshika31032000@gmail.com>
2020-12-25 22:30:49 +0530

    build config

--- accordian-app/src/Question.js
+++ accordian-app/src/Question.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import AddButton from "./AddButton";
 import SubtractButton from "./SubtractButton";
 const Question = ({ title, info }) => {
